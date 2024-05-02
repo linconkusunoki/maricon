7d913816e26ab00f99873c2c42be4d50d9fb6a38
Mari Martínez <43186548+maripng@users.noreply.github.com>
2024-05-02 11:27:06 +0200

    Update playlist.jsx

--- src/components/playlist.jsx
+++ src/components/playlist.jsx
@@ -32,7 +32,6 @@ const settings = {
 export function Playlist() {
   return (
     <Slider {...settings}>
-      {[1, 2, 3, 4, 5, 6].map((i) => (
         <div className="text-left " key={i}>
           <div key={i} className="aspect-square bg-earth" />
           <h3 className="mt-3 text-bronze font-primary">Anathema - Thin Air</h3>
@@ -45,7 +44,19 @@ export function Playlist() {
             Your browser does not support the audio element.
           </audio>
         </div>
-      ))}
+
+      <div className="text-left " key={i}>
+          <div key={i} className="aspect-square bg-[url('/img/tangerina.jpg')]" />
+          <h3 className="mt-3 text-bronze font-primary">Tangerina - Tiago Iorc</h3>
+          <p className="text-xs font-primary text-bronze">
+            Mari & Lincon / Our main songs
+          </p>
+
+          <audio controls className="w-full mt-3">
+            <source src="tangerina.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
     </Slider>
   );
 }
