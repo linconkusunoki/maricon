b839ceec980b8e59cec076bdb04ba80f6345c52b
Mari Martínez <43186548+maripng@users.noreply.github.com>
2024-05-03 09:52:02 +0200

    Update playlist.jsx

--- src/components/playlist.jsx
+++ src/components/playlist.jsx
@@ -33,7 +33,7 @@ export function Playlist() {
   return (
     <Slider {...settings}>
         <div className="text-left ">
-           <div className="bg-[url('/thinair.jpg')] aspect-[4/3] bg-cover bg-center" />
+           <div className="bg-[url('/thinair.jpeg')] aspect-[4/3] bg-cover bg-center" />
           <h3 className="mt-3 text-bronze font-primary">Thin Air - Anathema</h3>
           <p className="text-xs font-primary text-bronze">
             Mari & Lincon / Our main songs
@@ -54,6 +54,45 @@ export function Playlist() {
 
           <audio controls className="w-full mt-3">
             <source src="tangerina.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+      
+       <div className="text-left ">
+          <div className="bg-[url('/unfurl.jpg')] aspect-[4/3] bg-cover bg-center" />
+          <h3 className="mt-3 text-bronze font-primary">Unfurl - Katatonia</h3>
+          <p className="text-xs font-primary text-bronze">
+            Mari & Lincon / Our main songs
+          </p>
+
+          <audio controls className="w-full mt-3">
+            <source src="unfurl.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+
+      <div className="text-left ">
+          <div className="bg-[url('/sweaterweather.jpg')] aspect-[4/3] bg-cover bg-center" />
+          <h3 className="mt-3 text-bronze font-primary">Sweater Weather - The Neighbourhood</h3>
+          <p className="text-xs font-primary text-bronze">
+            Mari & Lincon / Our main songs
+          </p>
+
+          <audio controls className="w-full mt-3">
+            <source src="sweaterweather.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+
+      <div className="text-left ">
+          <div className="bg-[url('/songbird.jpg')] aspect-[4/3] bg-cover bg-center" />
+          <h3 className="mt-3 text-bronze font-primary">Songbird - Oasis</h3>
+          <p className="text-xs font-primary text-bronze">
+            Mari & Lincon / Our main songs
+          </p>
+
+          <audio controls className="w-full mt-3">
+            <source src="songbird.mp3" type="audio/mpeg" />
             Your browser does not support the audio element.
           </audio>
         </div>
