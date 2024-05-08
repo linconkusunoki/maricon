4da9d6184255868ed8edc41cdbeacda2cd0ecf81
Lincon Kusunoki <linconkusunoki@gmail.com>
2024-05-08 21:43:58 +0200

    nosso dia

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -93,6 +93,165 @@ export default function Home() {
 
       <Faq />
 
+      <section className="flex flex-col items-center gap-10 lg:gap-[70px] px-4 py-16 bg-green">
+        <div className="flex items-center justify-center">
+          <svg
+            width="56"
+            height="41"
+            viewBox="0 0 56 41"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="lg:w-[100px] lg:h-auto"
+          >
+            <path
+              d="M20.8645 34.6555C33.561 47.2773 55.4188 38.3063 55.4188 20.2829C55.4188 5.27586 39.7435 -4.35958 26.4446 1.97827C36.1313 1.58885 44.0833 9.33391 44.0833 18.909C44.0833 30.9515 31.9035 39.0568 20.8645 34.6555Z"
+              fill="#A25A44"
+            />
+            <path
+              d="M11.1792 14.0388C11.1792 9.42868 7.35061 5.6996 2.6866 5.88713C9.08965 2.8358 16.637 7.47497 16.637 14.7004C16.637 23.3782 6.11299 27.6974 0 21.6203C5.31491 23.7395 11.1792 19.8371 11.1792 14.0388Z"
+              fill="#A25A44"
+            />
+          </svg>
+
+          <h2 className="text-bronze font-secondary text-[32px] px-8 lg:text-[54px]">
+            Nosso dia
+          </h2>
+
+          <svg
+            width="56"
+            height="41"
+            viewBox="0 0 56 41"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="lg:w-[100px] lg:h-auto"
+          >
+            <path
+              d="M11.7545 18.909C11.7545 9.33385 19.7065 1.58879 29.3932 1.97821C16.0943 -4.3595 0.418945 5.2758 0.418945 20.283C0.418945 38.3064 22.2768 47.2774 34.9733 34.6555C23.9343 39.0568 11.7545 30.9515 11.7545 18.909ZM44.6586 14.0389C44.6586 9.42876 48.4872 5.69969 53.1512 5.88722C46.7481 2.83575 39.2008 7.47491 39.2008 14.7006C39.2008 23.3784 49.7248 27.6977 55.8378 21.6206C50.5229 23.7396 44.6586 19.8372 44.6586 14.0389Z"
+              fill="#A25A44"
+            />
+          </svg>
+        </div>
+
+        <ul className="relative flex flex-col gap-5 lg:flex-row">
+          <span className="absolute w-[2px] left-[8px] top-8 bottom-7 bg-[#A25A44] lg:left-11 lg:right-11 lg:bottom-[11px] lg:top-auto lg:w-auto lg:h-[2px]" />
+
+          <li className="flex items-center lg:flex-col-reverse lg:flex-1">
+            <svg
+              width="18"
+              height="24"
+              viewBox="0 0 18 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mt-1 mt-2 mr-4 lg:mr-0"
+            >
+              <path
+                d="M9.08136 0C9.57061 5.27496 13.4316 10.4956 17.9997 11.6919H18V11.7465C13.5951 12.9972 9.89741 18.0003 9.13597 23.0579H8.86403C7.99393 17.946 4.29619 13.0516 0 11.8009V11.7463C5.27496 10.6043 8.42885 5.32956 8.9727 0H9.08136Z"
+                fill="#A25A44"
+              />
+            </svg>
+
+            <div className="flex flex-col lg:text-center text-bronze lg:flex-col-reverse">
+              <h3 className="text-2xl font-semibold font-secondary">18:00h</h3>
+              <p className="text-[12px] font-primary">Chegada dos convidados</p>
+            </div>
+          </li>
+
+          <li className="flex items-center lg:flex-col-reverse lg:flex-1">
+            <svg
+              width="18"
+              height="24"
+              viewBox="0 0 18 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mt-1 mt-2 mr-4 lg:mr-0"
+            >
+              <path
+                d="M9.08136 0C9.57061 5.27496 13.4316 10.4956 17.9997 11.6919H18V11.7465C13.5951 12.9972 9.89741 18.0003 9.13597 23.0579H8.86403C7.99393 17.946 4.29619 13.0516 0 11.8009V11.7463C5.27496 10.6043 8.42885 5.32956 8.9727 0H9.08136Z"
+                fill="#A25A44"
+              />
+            </svg>
+
+            <div className="flex flex-col lg:text-center text-bronze lg:flex-col-reverse">
+              <h3 className="text-2xl font-semibold font-secondary">18:30h</h3>
+              <p className="text-[12px] font-primary">Cerimônia</p>
+            </div>
+          </li>
+
+          <li className="flex items-center lg:flex-col-reverse lg:flex-1">
+            <svg
+              width="18"
+              height="24"
+              viewBox="0 0 18 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mt-1 mt-2 mr-4 lg:mr-0"
+            >
+              <path
+                d="M9.08136 0C9.57061 5.27496 13.4316 10.4956 17.9997 11.6919H18V11.7465C13.5951 12.9972 9.89741 18.0003 9.13597 23.0579H8.86403C7.99393 17.946 4.29619 13.0516 0 11.8009V11.7463C5.27496 10.6043 8.42885 5.32956 8.9727 0H9.08136Z"
+                fill="#A25A44"
+              />
+            </svg>
+
+            <div className="flex flex-col lg:text-center text-bronze lg:flex-col-reverse">
+              <h3 className="text-2xl font-semibold font-secondary">19:30h</h3>
+              <p className="text-[12px] font-primary">Cocktails</p>
+            </div>
+          </li>
+
+          <li className="flex items-center lg:flex-col-reverse lg:flex-1">
+            <svg
+              width="18"
+              height="24"
+              viewBox="0 0 18 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mt-1 mt-2 mr-4 lg:mr-0"
+            >
+              <path
+                d="M9.08136 0C9.57061 5.27496 13.4316 10.4956 17.9997 11.6919H18V11.7465C13.5951 12.9972 9.89741 18.0003 9.13597 23.0579H8.86403C7.99393 17.946 4.29619 13.0516 0 11.8009V11.7463C5.27496 10.6043 8.42885 5.32956 8.9727 0H9.08136Z"
+                fill="#A25A44"
+              />
+            </svg>
+
+            <div className="flex flex-col lg:text-center text-bronze lg:flex-col-reverse">
+              <h3 className="text-2xl font-semibold font-secondary">21:00h</h3>
+              <p className="text-[12px] font-primary">Jantar & Brindes</p>
+            </div>
+          </li>
+
+          <li className="flex items-center lg:flex-col-reverse lg:flex-1">
+            <svg
+              width="18"
+              height="24"
+              viewBox="0 0 18 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mt-1 mt-2 mr-4 lg:mr-0"
+            >
+              <path
+                d="M9.08136 0C9.57061 5.27496 13.4316 10.4956 17.9997 11.6919H18V11.7465C13.5951 12.9972 9.89741 18.0003 9.13597 23.0579H8.86403C7.99393 17.946 4.29619 13.0516 0 11.8009V11.7463C5.27496 10.6043 8.42885 5.32956 8.9727 0H9.08136Z"
+                fill="#A25A44"
+              />
+            </svg>
+
+            <div className="flex flex-col lg:text-center text-bronze lg:flex-col-reverse">
+              <h3 className="text-2xl font-semibold font-secondary">22:30h</h3>
+              <p className="text-[12px] font-primary">Festa</p>
+            </div>
+          </li>
+        </ul>
+
+        <p className="font-primary text-bronze lg:max-w-[1100px] lg:mx-auto lg:text-center">
+          Caso queira fazer um brinde ou qualquer outra surpresa para nós, nossa
+          coordenadora, Sandra, vai cuidar de tudo e pode te auxiliar em
+          encontrar o melhor momento pra isso.
+          <br />
+          <a className="font-bold underline" href="tel:1231231323">
+            Entrar em contato →
+          </a>
+        </p>
+      </section>
+
       <section className="px-4 py-16 text-center bg-bronze">
         <h2 className="text-3xl text-green font-secondary xl:text-5xl">
           {t.countdown_title}
